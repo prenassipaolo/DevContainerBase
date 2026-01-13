1b79a9012d5a6802b237d56b7f333bb764598cd8
prenassipaolo <prenassipaolo@gmail.com>
2026-01-13 19:27:05 +0000

    wip: add logging in post create

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Sync all UV groups and activate the virtual environment
+echo "Synchronizing UV environment..."
 uv sync --frozen --group dev --group test --no-install-project --active
 if [ $? -ne 0 ]; then
     echo "Failed to synchronize UV environment."
     exit 1
 fi
+echo "Activating virtual environment..."
 source .venv/bin/activate
